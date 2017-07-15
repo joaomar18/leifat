2f32a58ee5b75c35b542235b5490de5534f7e724
prokiller1800 <joaomarques13eac@epms.pt>
2017-07-15 13:38:50 +0100

    LOL

--- logic.js
+++ logic.js
@@ -8,8 +8,10 @@ var preco_capacitiva = document.getElementById("c_erc");
 
 //Inputs Selectors
 
-var tarifa = document.getElementById("t.c");
-var fatura = document.getElementById("t.f");
+var custo_e1 = document.getElementById("custo_e1");
+var custo_e2 = document.getElementById("custo_e2");
+var custo_e3 = document.getElementById("custo_e3");
+var custo_ehv = document.getElementById("custo_ecap");
 
 //Results
 
@@ -25,31 +27,18 @@ var custo = document.getElementById("c_er");
 
 function calcular(){
 
-var tar = Number(Number(tarifa.value));
-var fat = Number(Number(fatura.value));
-
 var ativa_fv= Number(Number(energia_ativa_fv.value));
 var ativa_hv= Number(Number(energia_ativa_hv.value));
 var escalao1=  Number(Number(preco_escalao1.value));
 var escalao2=  Number(Number(preco_escalao2.value));
 var escalao3=  Number(Number(preco_escalao3.value));
 
-var fator_tempo;
+var custo_escalao1 = Number(Number(custo_e1.value));
+var custo_escalao2 = Number(Number(custo_e2.value));
+var custo_escalao3 = Number(Number(custo_e3.value));
+var custo_escalaohv = Number(Number(custo_ehv.value));
+
 var nao_acionar=false;
-
-
-
-if(fat==0){
-fator_tempo=(30.42*3);
-}
-if(fat==1){
-fator_tempo=(30.42);
-}
-if(fat==2){
-fator_tempo=(30.42*2);
-}
-
-
 
 var preco_reativa_fv;
 var preco_reativa_hv;
@@ -58,22 +47,29 @@ var valor_reativa_total;
 var valor_reativa_escalao=0;
 var valor_numero_reativa_total;
 
-if(tar==0){   //BTE
-preco_reativa_fv= 0.0346;
-preco_reativa_hv= 0.0264;
+
+if((custo_escalao1==0 || custo_escalao2==0 || custo_escalao3==0) && custo_escalaohv==0){
+alert("Insira os preços dos escalões!");
+custo.value="";
+reativa_capacitiva_total.value="";
+reativa_indutiva_total.value="";
+cos_fi_fv.value="";
+cos_fi_hv.value="";
+nao_acionar=true;
+}
+if(custo_escalao1<0 || custo_escalao2<0 || custo_escalao3<0 || custo_escalaohv<0){
+alert("Preços de escalões inválidos!");
+custo.value="";
+reativa_capacitiva_total.value="";
+reativa_indutiva_total.value="";
+cos_fi_fv.value="";
+cos_fi_hv.value="";
+nao_acionar=true;
 }
 
-if(tar==1){  //MT
-preco_reativa_fv= 0.0290;
-preco_reativa_hv= 0.0218;
-}
-
-if(tar==2 || tar==3){  //AT && MT
-preco_reativa_fv= 0.0267;
-preco_reativa_hv= 0.02;
-}
 
 
+if(custo_escalao1>0 && custo_escalao2>0 && custo_escalao3>0){
 
 if(ativa_fv==0 && ativa_hv==0){
 custo.value="";
@@ -110,36 +106,36 @@ if((ativa_fv>0 || ativa_hv>0) && nao_acionar==false) {
 //Horas Fora de Vazio
 
 if(escalao1==0 && (escalao2>0 || escalao3>0)){
-valor_reativa_total="Custos de Escalão Inválidos!";
+valor_reativa_total="Custos Escalão Inválidos!";
 nao_acionar=true;
 }
 if(escalao2==0 && escalao3>0){
-valor_reativa_total="Custos de Escalão Inválidos!";
+valor_reativa_total="Custos Escalão Inválidos!";
 nao_acionar=true;
 }
 if(escalao1<0 || escalao2<0 || escalao3<0){
-valor_reativa_total="Custos de Escalão Inválidos!";
+valor_reativa_total="Custos Escalão Inválidos!";
 nao_acionar=true;
 }
 
 if(escalao1==0 && escalao2==0 && escalao3==0){
-valor_reativa_total=("<="+((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.96)))+"/dia");
+valor_reativa_total=("<="+((ativa_fv)*Math.tan(Math.acos(0.96))));
 valor_numero_reativa_total=0;
 }
 if(escalao1>0 && escalao2==0 && escalao3==0){
-valor_reativa_escalao=(escalao1/fator_tempo)/(preco_reativa_fv*(1/3));
-valor_reativa_total= (valor_reativa_escalao + ((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.96)))+"/dia");
-valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.96))));
+valor_reativa_escalao=(escalao1)/(custo_escalao1);
+valor_reativa_total= (valor_reativa_escalao + ((ativa_fv)*Math.tan(Math.acos(0.96))));
+valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv)*Math.tan(Math.acos(0.96))));
 }
 if(escalao2>0 && escalao3==0 && escalao1>0){
-valor_reativa_escalao=(escalao2/fator_tempo)/(preco_reativa_fv*(1));
-valor_reativa_total= (valor_reativa_escalao + ((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.93)))+"/dia");
-valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.93))));
+valor_reativa_escalao=(escalao2)/(custo_escalao2);
+valor_reativa_total= (valor_reativa_escalao + ((ativa_fv)*Math.tan(Math.acos(0.93))));
+valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv)*Math.tan(Math.acos(0.93))));
 }
 if(escalao3>0 && escalao2>0 && escalao1>0){
-valor_reativa_escalao=(escalao3/fator_tempo)/(preco_reativa_fv*(3));
-valor_reativa_total= (valor_reativa_escalao + ((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.89)))+"/dia");
-valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv/fator_tempo)*Math.tan(Math.acos(0.89))));
+valor_reativa_escalao=(escalao3)/(custo_escalao3);
+valor_reativa_total= (valor_reativa_escalao + ((ativa_fv)*Math.tan(Math.acos(0.89))));
+valor_numero_reativa_total=(valor_reativa_escalao+((ativa_fv)*Math.tan(Math.acos(0.89))));
 }
 
 
@@ -163,7 +159,7 @@ cosfi_em_fora_vazio="Não Existente!";
 }
 }
 if(valor_numero_reativa_total>=0 && ativa_fv>0){
-cosfi_em_fora_vazio= Math.cos(Math.atan(valor_numero_reativa_total/(ativa_fv/fator_tempo)));
+cosfi_em_fora_vazio= Math.cos(Math.atan(valor_numero_reativa_total/(ativa_fv)));
 }
 
 cos_fi_fv.value=cosfi_em_fora_vazio;
@@ -174,18 +170,23 @@ else{
 cos_fi_fv.value="Dados Insuficientes";
 nao_acionar=true;
 }
-
+}
+}
+else{
+reativa_indutiva_total.value="";
+cos_fi_fv.value="";
+}
 //Fim Horas F.V.
 
 
-
+if(custo_escalaohv>0){
 
 var custo_capacitiva=Number(Number(preco_capacitiva.value));
 
 
-var energia_reativa_capacitiva= (custo_capacitiva/fator_tempo)/preco_reativa_hv;
-var cosfi_em_vazio= Math.cos(Math.atan(energia_reativa_capacitiva/(ativa_hv/fator_tempo)));
-reativa_capacitiva_total.value=(energia_reativa_capacitiva+"/dia");
+var energia_reativa_capacitiva= (custo_capacitiva)/custo_escalaohv;
+var cosfi_em_vazio= Math.cos(Math.atan(energia_reativa_capacitiva/(ativa_hv)));
+reativa_capacitiva_total.value=(energia_reativa_capacitiva);
 
 if(energia_reativa_capacitiva>=0){
 cosfi_hv.value=cosfi_em_vazio;
@@ -193,8 +194,11 @@ cosfi_hv.value=cosfi_em_vazio;
 if(ativa_hv==0){
 cosfi_hv.value="Não Existente";
 }
-
-
+}
+else{
+reativa_capacitiva_total.value="";
+cos_fi_hv.value="";
+}
 
 
 if(nao_acionar==false){
@@ -210,12 +214,18 @@ if(ativa_fv>0 && ativa_hv>0){
 custo.value=escalao1+escalao2+escalao3+custo_capacitiva;
 }
 }
-if(nao_acionar==true && (ativa_hv>0 || ativa_fv>0)){
+if(nao_acionar==true){
+if(ativa_hv>0 || ativa_fv>0){
 custo.value="Dados Inválidos!";
 }
+if((custo_escalao1==0 || custo_escalao2==0 || custo_escalao3==0) && custo_escalaohv==0){
+custo.value="";
+}
+else{
+custo.value="";
 }
 }
-
+}
 
 //Function RollDown
 
